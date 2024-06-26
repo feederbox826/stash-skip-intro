620f350afaac235cdd3752d26793bc88decd3afc
feederbox826 <feederbox826@users.noreply.github.com>
2024-06-26 05:25:44 -0400

    add FuckYouCash studios

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # stash-skip intro database
 
 database for [skip-intro](https://github.com/feederbox826/plugins/tree/main/plugins/skip-intro) plugin
+
+in the case of any historical changes, the most recent and consistent end time will be taken.
+
+feel free to make requests in the [issues](https://github.com/feederbox826/stash-skip-intro/issues) or [add them yourself](https://github.com/feederbox826/stash-skip-intro/edit/main/intros.js)
--- intros.js
+++ intros.js
@@ -1,5 +1,19 @@
 // intros
+// "studioid": end of intro in seconds.ms // example.com
 const intros = {
-    // "studioid": end of intro in seconds.ms
-    "1f711e88-f4d0-4ba8-8035-d365b7e6de64": 15 // Anal4K
+    "1f711e88-f4d0-4ba8-8035-d365b7e6de64": 15, // anal4k.com
+    "885b33d1-82b5-48de-931f-f3c13fdf8188": 12, // castingcouch-x.com
+    "ba5a29d0-d797-4618-b9dd-c9059ab71d43": 14, // cum4k.com
+    "e1e14677-ac40-4e52-824f-4c734ac4950b": 14, // facials4k.com, varies
+    "26078c8c-b0aa-4744-995b-968d282ed310": 9, // fantasyhd.com
+    "ca3d3e01-5f46-4a00-be06-03f10650b3ab": 14.1, // girlcum.com
+    "5ba69962-79cc-40d4-9971-3ff8038fac14": 15, // holed.com
+    "63c9716d-3309-4663-ba68-259fa87f82b1": 14, // lubed.com
+    "3cb7aacf-9e13-4b83-94ef-1e70978a54cb": 7, // myveryfirsttime.com
+    "6bc48f3a-999e-4a04-badd-07ae22d94661": 18.1, // nannyspy.com
+    "bd31010f-ce5b-4c68-bbe6-f3eda74537b0": 11, // passion-hd.com
+    "4e11a55f-ca6b-4c2d-9d48-b3c475ab8382": 13.7, // povd.com
+    "842cc5ba-4f5c-4bba-bc4e-04637f4d5c13": 16.6, // spyfam.com
+    "a1a6358e-8141-4d19-9f9c-5e233f838b71": 7.1, // tiny4k.com
+    "981887d6-da48-4dfc-88d1-7ed13a2754f2": 10.1, // wetvr.com
 }
