fba83adc4af95a436781f54c6b5c823c59952620
feederbox826 <feederbox826@users.noreply.github.com>
2024-06-26 22:27:47 -0400

    add nubiles-porn

--- intros.js
+++ intros.js
@@ -4,12 +4,15 @@ const intros = {
     "1f711e88-f4d0-4ba8-8035-d365b7e6de64": 15, // anal4k.com
     "1f8e88b5-6589-47c2-87b6-ad398ef71cdf": 45.4, // myfamilypies.com
     "26078c8c-b0aa-4744-995b-968d282ed310": 9, // fantasyhd.com
+    "2b7a36ed-cd0f-43d5-95ad-354bfc37469d": 34.1, // detentiongirls.com
     "33cfbe40-6d8e-41c3-9d09-0d64a4f69add": 28, // princesscum.com
     "3b1e408c-e111-4fe4-aa86-379ccc83b3cc": 7, // 3rddegreefilms.com
     "3b88b263-ac4f-4fc6-8031-38daa05d452d": 22.5, // stepsiblingscaught
     "3cb7aacf-9e13-4b83-94ef-1e70978a54cb": 7, // myveryfirsttime.com
+    "31643c64-6da3-46d6-8507-69e34c601b48": 25, // cumswappingsis.com
     "4e11a55f-ca6b-4c2d-9d48-b3c475ab8382": 13.7, // povd.com
     "51db34a6-3d0f-4052-99bd-49fa7601240a": 24, // petitehdporn.com
+    "570b024d-0c2c-4193-8801-a887cec7ea68": 15, // lilsis.com
     "57f8a10e-86f0-42ee-b486-09b43ca2a194": 20.5, // nubileset.com
     "5ba69962-79cc-40d4-9971-3ff8038fac14": 15, // holed.com
     "63c9716d-3309-4663-ba68-259fa87f82b1": 14, // lubed.com
@@ -17,11 +20,14 @@ const intros = {
     "6eb270c7-6d95-41ae-a5a5-b6f607229463": 4, // badoink hd
     "70f0d8de-3aa7-4fa0-8b14-cfed49f1fa8f": 8, // badoinkvr.com
     "842cc5ba-4f5c-4bba-bc4e-04637f4d5c13": 16.6, // spyfam.com
+    "86f205b2-adee-4996-b303-aa11e8413b4b": 26, // daddyslilangel.com
     "885b33d1-82b5-48de-931f-f3c13fdf8188": 12, // castingcouch-x.com
     "90e72086-7128-4500-8cd3-9d1956aa7cae": 11, // bannedstories.com
     "981887d6-da48-4dfc-88d1-7ed13a2754f2": 10.1, // wetvr.com
     "99ab253e-c5c3-4289-9088-5d3c66f78c51": 6, // bamvisions.com
-    "a1a6358e-8141-4d19-9f9c-5e233f838b71": 7.1, // tinyrk.com
+    "a1742003-0dde-4b69-9052-88b67bf1b001": 22.4, // cheatingsis.com
+    "a1a6358e-8141-4d19-9f9c-5e233f838b71": 7.1, // tiny4k.com
+    "b05b6376-2019-4c7d-856e-17bbf40480b3": 16, // badteenspunished.com
     "b137c40b-8c55-42c1-9bdd-df90a4955017": 18, // smashed.xxx
     "b98568e4-4477-43c2-b0e1-09a5296646f1": 6, // 5kporn.com
     "ba5a29d0-d797-4618-b9dd-c9059ab71d43": 14, // cum4k.com
@@ -36,4 +42,5 @@ const intros = {
 const networks = {
     "2be8463b-0505-479e-a07d-5abc7a6edd54": 13.1, // naughtyamerica.com
     "525f8c32-d14f-42c0-939c-bb5d8eae8bcf": 7.1, // bangbros.com
+    "eec95cdd-9f58-4fc7-b7d1-e98786453d27": 13, // brazzers.com
 }
